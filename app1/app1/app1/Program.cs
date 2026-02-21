de734f8e8894264c4c8e50ff9ee1e6d255e52697
Mohamed-Emira-001 <emiradev909@gmail.com>
2026-02-21 03:46:23 +0200

    first commit

---
+++ app1/app1/app1/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+///////Console Calulator///////////
+//Welcome Message
+Console.WriteLine("Hello");
+Console.WriteLine("What is Your Name ?");
+//Take the User_Name (Input) 
+string userInput = Console.ReadLine();
+Console.WriteLine($"Welcome {userInput}\n Okay Choose Your First Number");
+//Take the firstNumber (Input)
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"First number is {num1}\n Now Choose Your Second Number");
+//Take the secondNumber (Input)
+int num2 = int.Parse(Console.ReadLine());
+//Take the CHOOSEN arthimitic Operation (* / + -)
+Console.WriteLine($"Second number is {num2}\n Now Choose Your Second Number\n Now choose your arthimtic operation whether is (* / + -)");
+char arhtOperator = char.Parse(Console.ReadLine());
+Console.WriteLine($"Your Choosen arthimatic Operator is {arhtOperator}\n");
+//Intalize a variable
+int result;
+/////* The Calculation by making if-condition */////
+
+if (arhtOperator == '*') // if the user chooses *
+{
+    result = num1 * num2;
+    Console.WriteLine(result);
+}else if (arhtOperator == '/') //if the user chooses /
+{
+    result = num1 / num2;
+    Console.WriteLine(result);
+
+    Console.WriteLine(result);
+}else if (arhtOperator == '+') // if the user chooses +
+{
+    result = num1 + num2;
+    Console.WriteLine(result);
+}else{
+    result = num1 - num2; //if the user chooses -
+    Console.WriteLine(result);
+     }
+
+//End Message
+Console.WriteLine("BYE BYE!!");
---
+++ quiz_app/quiz_app/Program.cs
@@ -0,0 +1,41 @@
+﻿//// Do you know me well ?? ////
+using System.ComponentModel.Design;
+
+string[] questions ={"What is my name?","What is my age?","Where am i from?"};
+string[] answers ={"emira","youssef","yassin","20","27","32","morroco","italy","egypt"};
+
+//Intalize Score 
+int score = 0;
+Console.WriteLine("Welcome to (Do you know me Program)\nChoose the Correct Answer");
+//First Question
+Console.WriteLine($"First Question:{questions[0]}\n A:{answers[0]}\n B:{answers[1]}\n C:{answers[2]}");
+string user_answer1 =Console.ReadLine();
+//Second Question
+Console.WriteLine($"Second Question:{questions[1]}\n A:{answers[3]}\n B:{answers[4]}\n C:{answers[5]}");
+string user_answer2 = Console.ReadLine();
+//Third Question
+Console.WriteLine($"Third Question:{questions[2]}\n A:{answers[6]}\n B:{answers[7]}\n C:{answers[8]}");
+string user_answer3 = Console.ReadLine();
+
+
+//validate the answers
+if (user_answer1 == answers[0])
+{
+    score++;
+    Console.WriteLine($"Your answer is correct your score is {score}");
+}
+else if (user_answer2 == answers[4])
+{
+    score++;
+    Console.WriteLine($"Your answer is correct your score is {score}");
+}
+else if (user_answer3 == answers[8])
+{
+    score++;
+    Console.WriteLine($"Your answer is correct your score is {score}");
+}
+else
+{
+    score--;
+    Console.WriteLine($"Your answer is wrong your score is {score}");
+}
